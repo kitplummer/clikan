054c40aa911f323eba6a7fbfedf1a630a496a5ae
Kit Plummer <kitplummer@gmail.com>
2017-06-01 09:10:21 -0700

    update installer

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup (
         install_requires=[
             'Click',
             'pyyaml',
-            'beautifultable'
+            'terminaltables',
+            'textwrap'
         ],
         entry_points='''
             [console_scripts]
