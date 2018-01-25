bf9403b577e20191eb5de6df14abc56ed00f83bf
Kit Plummer <kitplummer@gmail.com>
2018-01-24 21:32:32 -0500

    update setup

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 
 setup (
+        author="Kit Plummer",
+        author_email="kitplummer@gmail.com",
         name="clikan",
         version='0.0.1',
         py_modules=['clikan'],
@@ -12,5 +14,11 @@ setup (
         entry_points='''
             [console_scripts]
             clikan=clikan:cli
-        '''
+        ''',
+        classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Environment :: Console"
+        ]
 )
