3fee38f6cdce296c629b8f2bc3b1ba5bd27f9064
kitplummer <kit.plummer@gtri.gatech.edu>
2020-04-15 11:10:29 -0400

    update: setup.py configuration with new description field

--- setup.py
+++ setup.py
@@ -3,26 +3,31 @@ import os
 version_file = open(os.path.join('./', 'VERSION'))
 version = version_file.read().strip()
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup (
-        author="Kit Plummer",
-        author_email="kitplummer@gmail.com",
-        name="clikan",
-        version=version,
-        description="Simple CLI-based Kanban board",
-        py_modules=['clikan'],
-        install_requires=[
-            'Click',
-            'pyyaml',
-            'terminaltables'
-        ],
-        entry_points='''
-            [console_scripts]
-            clikan=clikan:clikan
-        ''',
-        classifiers=[
-            "License :: OSI Approved :: MIT License",
-            "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3",
-            "Environment :: Console"
-        ]
+    author="Kit Plummer",
+    author_email="kitplummer@gmail.com",
+    name="clikan",
+    version=version,
+    description="Simple CLI-based Kanban board",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=['clikan'],
+    install_requires=[
+        'Click',
+        'pyyaml',
+        'terminaltables'
+    ],
+    entry_points='''
+        [console_scripts]
+        clikan=clikan:clikan
+    ''',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Environment :: Console"
+    ]
 )
