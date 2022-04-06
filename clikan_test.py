15b248f78fc662dbddc546aba85a3c6b08978ed8
Oday Mansour <odaym@nsour.net>
2022-04-06 21:25:57 +0100

    Adding option for maximum task label length (+tests)

--- clikan_test.py
+++ clikan_test.py
@@ -125,7 +125,6 @@ def test_repaint_config_option():
     version_file = open(os.path.join('./', 'VERSION'))
     version = version_file.read().strip()
     path_to_config = str(pathlib.Path("./tests/repaint").resolve())
-    print(path_to_config)
     with runner.isolation(
         input=None,
         env={"CLIKAN_HOME": path_to_config},
@@ -145,7 +144,6 @@ def test_no_repaint_config_option():
     version_file = open(os.path.join('./', 'VERSION'))
     version = version_file.read().strip()
     path_to_config = str(pathlib.Path("./tests/no_repaint").resolve())
-    print(path_to_config)
     with runner.isolation(
         input=None,
         env={"CLIKAN_HOME": path_to_config},
@@ -158,3 +156,41 @@ def test_no_repaint_config_option():
         assert result.exit_code == 0
         assert 'n_--task_test' in result.output
         assert version not in result.output
+
+
+# Limit on task name length tests
+
+def test_taskname_config_option():
+    runner = CliRunner()
+    version_file = open(os.path.join('./', 'VERSION'))
+    version = version_file.read().strip()
+    path_to_config = str(pathlib.Path("./tests/taskname").resolve())
+    with runner.isolation(
+        input=None,
+        env={"CLIKAN_HOME": path_to_config},
+        color=False
+    ):
+        result = runner.invoke(clikan, [])
+        assert result.exit_code == 0
+        assert 'clikan' in result.output
+        result = runner.invoke(clikan, ["a", "This is a long task name, more than 40 characters (66 to be exact)"])
+        assert result.exit_code == 0
+        assert 'This is a long task name, more than 40 characters (66 to be exact)' in result.output
+
+
+def test_no_taskname_config_option():
+    runner = CliRunner()
+    version_file = open(os.path.join('./', 'VERSION'))
+    version = version_file.read().strip()
+    path_to_config = str(pathlib.Path("./tests/no_taskname").resolve())
+    with runner.isolation(
+        input=None,
+        env={"CLIKAN_HOME": path_to_config},
+        color=False
+    ):
+        result = runner.invoke(clikan, [])
+        assert result.exit_code == 0
+        assert 'clikan' in result.output
+        result = runner.invoke(clikan, ["a", "This is a long task name, more than 40 characters (66 to be exact)"])
+        assert result.exit_code == 0
+        assert 'Brevity counts.' in result.output
