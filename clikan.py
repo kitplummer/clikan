18f9657cc3c7418971f47a437f688c44823a2e61
Kit Plummer <kit@ditto.live>
2024-03-08 15:43:03 -0500

    fix: importlib and MANIFEST.in for VERSION

--- clikan.py
+++ clikan.py
@@ -10,10 +10,9 @@ from textwrap import wrap
 import collections
 import datetime
 import configparser
-import pkg_resources  # part of setuptools
+import importlib
 
-VERSION = pkg_resources.require("clikan")[0].version
-
+VERSION = importlib.metadata.version('clikan')
 
 class Config(object):
     """The config in this example only holds aliases."""
